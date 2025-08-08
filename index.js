7b659d82b63e2034368a3b6126984dd47a04f2d0
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-08-08 12:17:14 +0200

    setup static files and their delivery

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+// 	res.send('Hello World!');
+// });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
