a94a14d41895b42db611e1c5d6eb7020a69ec759
Rakinzi Silver <silverrakinzi@gmail.com>
2025-08-14 14:55:48 +0200

    Added another env variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Load environment variables from .env file
 function loadEnv() {
